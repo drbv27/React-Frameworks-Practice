d3f251141bc7081dd20c1ebe1616bd8152486393
Diego <drbv27@gmail.com>
2021-08-12 06:10:45 -0500

    Using ReactBootstrap components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Bootstrap from "./components/Bootstrap";
+//import Bootstrap from "./components/Bootstrap";
+import ReactBootstrap from "./components/ReactBootstrap";
+//import Bulma from "./components/Bulma";
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS con REACT</h1>
+      {/* <Bulma /> */}
       {/* <Bootstrap /> */}
+      <ReactBootstrap />
     </div>
   );
 }
